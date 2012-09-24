b1f30cf63a0f7267902da109a1bae4f7bb7098b6
Bobby Smith <reversetype@gmail.com>
2012-09-24 16:21:41 -0400

    modify responsive bar to not make it look like part of the site

--- header.php
+++ header.php
@@ -59,13 +59,29 @@
 
 <?php wp_head(); ?>
 </head>
+  <?php
+  
+  add_filter( 'body_class', 'my_neat_body_class');
+  function my_neat_body_class( $classes ) {
+        $classes[] = 'responsive-help';
+       return $classes;
+  }
+  
+  ?>
 
 <body <?php body_class(); ?>>
+  
+  
+  
+  
+  
+  
+  
 <div id="page" class="hfeed site wrap">
  <?php do_action( 'before' ); ?>
  <header id="masthead" class="site-header" role="banner">
 	 <hgroup>
-		 <h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		 <h1 class="site-title"><abbr href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 	 </hgroup>
 
 	 <nav role="navigation" class="site-navigation main-navigation">
