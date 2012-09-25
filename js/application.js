067fa356e546f100f9c3f771c25b1bced4126a22
Andre Fredette <andre@reality66.com>
2012-09-25 12:15:14 -0400

    only do signup scroll on pages where it exists

--- js/application.js
+++ js/application.js
@@ -1,25 +1,30 @@
 	(function($){
 		$(document).ready(function(){
-			
-	    $(window).scroll(function() {						
-				  var browserbottom = $(window).scrollTop() + $(window).height();
-					var offset = $("#signup-scroll-target").offset();
-					var height = offset.top - 30;
+		
+			if($("#signup-scroll-target").length > 0){
+				// only do signup scroll if its on the page
 				
-					//console.log("scrollTop: " + $(window).scrollTop() + " browserbottom: " + browserbottom + " height: " + height)
+				$(window).scroll(function() {						
+					  var browserbottom = $(window).scrollTop() + $(window).height();
+						var offset = $("#signup-scroll-target").offset();
+						var height = offset.top - 30;
+				
+						//console.log("scrollTop: " + $(window).scrollTop() + " browserbottom: " + browserbottom + " height: " + height)
 					
-	        if (browserbottom >= height) {
-	            $("#sign-up").removeClass('fixed');
-	        } else {
-	            $("#sign-up").addClass('fixed');
-	        }
-	    });
+		        if (browserbottom >= height) {
+		            $("#sign-up").removeClass('fixed');
+		        } else {
+		            $("#sign-up").addClass('fixed');
+		        }
+		    });
 	
-			$("#sign-up").click(function(){
-				offset = $("#signup-scroll-target").offset();
-				height = offset.top - 30;
-				$('html, body').animate({scrollTop: (height - $(window).height()) + 165},'fast');
-			})
+				$("#sign-up").click(function(){
+					offset = $("#signup-scroll-target").offset();
+					height = offset.top - 30;
+					$('html, body').animate({scrollTop: (height - $(window).height()) + 165},'fast');
+				})
+			}	
+	    
 			
 		})
 	})(jQuery);
