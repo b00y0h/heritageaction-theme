34e8669cef50a2ef8295e721e7c038e948206821
Andre Fredette <andre@reality66.com>
2012-10-04 18:20:14 -0400

    Removed disqus widget (it didn't work)

--- sidebar.php
+++ sidebar.php
@@ -35,18 +35,6 @@
 		    </aside>
 		    
 		    <aside id="disqus-comments" class="widget">
-		      <script type="text/javascript">
-                 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                 var disqus_shortname = 'haction'; // required: replace example with your forum shortname
-
-                 /* * * DON'T EDIT BELOW THIS LINE * * */
-                 (function () {
-                     var s = document.createElement('script'); s.async = true;
-                     s.type = 'text/javascript';
-                     s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-                     (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-                 }());
-                 </script>
 			    <img style="width:100%" src="<?php echo get_bloginfo('template_url'); ?>/img/disqus-plugin-widget.gif" alt="Disqus Plugin Widget">
 			  </aside>
 			  
