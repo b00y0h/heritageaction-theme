cfdf65fce7bca581dc9168ac9d84522c5ba26977
Bobby Smith <reversetype@gmail.com>
2012-10-24 02:04:20 -0400

    FIX [7] social buttons in sidebar

--- header.php
+++ header.php
@@ -60,12 +60,6 @@
 
 <?php wp_head(); ?>
 
-<style type="text/css" media="screen">
-  .twitter-widget-share{
-    float:right;
-  }
-</style>
-
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
