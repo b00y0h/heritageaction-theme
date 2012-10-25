136ded0aa2ee757f785d697fd049984263d87d61
Bobby Smith <reversetype@gmail.com>
2012-10-25 15:04:32 -0400

    cleaned up js (with JSHINT) and added detectmobilebrowser.js

--- js/application.js
+++ js/application.js
@@ -1,133 +1,183 @@
-	(function($){
-		$(document).ready(function(){
-			
-			// press release slider
-			$("#press-release-slider").tinycarousel({ 
-        pager: true, 
+/**
+ *
+ * Application File
+ *
+ **/
+
+
+// Placing functions before they are called (JSHINT)
+ function actionDashboardGo(){
+    jQuery(".dashboardZipSearch").removeClass('error');
+    var context;
+    if(jQuery("li.dashboard").css('text-shadow') !== 'none'){
+     context = jQuery("li.dashboard");
+    }
+    else{
+     context = jQuery("#action-dashboard");
+    }
+     if(jQuery(".dashboardZipSearch", context).val() !== '' && jQuery(".dashboardZipSearch", context).val() !== "Enter your zip"){
+        window.location = '/congress/' + jQuery(".dashboardZipSearch", context).val();
+     }
+     else{
+        jQuery(".dashboardZipSearch", context).addClass('error');
+     }
+  }
+
+  function getCookie(name){
+ var nameEQ = name + "=";
+   var ca = document.cookie.split(';');
+   for(var i=0;i < ca.length;i++) {
+     var c = ca[i];
+     while (c.charAt(0) ===' ') {c = c.substring(1,c.length);}
+     if (c.indexOf(nameEQ) === 0) {return c.substring(nameEQ.length,c.length);}
+   }
+   return null;
+  }
+  function setCookie(name, value, days){
+    var expires;
+  if (days) {
+   var date = new Date();
+   date.setTime(date.getTime()+(days*24*60*60*1000));
+   expires = "; expires="+date.toGMTString();
+  }
+  else { expires = "";}
+  document.cookie = name+"="+value+expires+"; path=/";
+  }
+
+
+
+
+  (function($){
+    $(document).ready(function(){
+
+      // press release slider
+      $("#press-release-slider").tinycarousel({
+        pager: true,
         interval: true,
         intervaltime: 7000
       });
-			
-			// home key votes toggle
-			$(".keyVoteSelector").change(function(){
-				$(".items").slideUp();
-				$(".items_" + $(".keyVoteSelector:checked").val() ).slideDown();
-				$("#more-key-votes-link").attr('href','/chamber/key-vote-' + $(".keyVoteSelector:checked").val() + '/')
-			})
-			
-			$("#search-trigger").click(function(){
-				if($("#search").hasClass('hover')){
-					$("#search").removeClass('hover');
-				}
-				else if(!$("#search").hasClass('hover')){
-					$("#search").addClass('hover');
-				}
-			})
 
-			if($("#signup-scroll-target").length > 0){
-				// only do signup scroll if its on the page
+      // home key votes toggle
+      $(".keyVoteSelector").change(function(){
+        $(".items").slideUp();
+        $(".items_" + $(".keyVoteSelector:checked").val() ).slideDown();
+        $("#more-key-votes-link").attr('href','/chamber/key-vote-' + $(".keyVoteSelector:checked").val() + '/');
+      });
 
-				$(window).scroll(function() {
-						var browserbottom = $(window).scrollTop() + $(window).height();
-						var offset = $("#signup-scroll-target").offset();
-						var height = offset.top - 30;
-
-						//console.log("scrollTop: " + $(window).scrollTop() + " browserbottom: " + browserbottom + " height: " + height)
-
-						if (browserbottom >= height) {
-								$("#sign-up").removeClass('fixed');
-						} else {
-								$("#sign-up").addClass('fixed');
-						}
-				});
-
-				$("#sign-up-tab").click(function(){
-					var offset = $("#signup-scroll-target").offset();
-					var height = offset.top - 30;
-					$('html, body').animate({scrollTop: (height - $(window).height()) + 165},'fast');
-				});
-			}
-			
-			$(".dashboardZipGo").click(function(){
-        actionDashboardGo();
-				return false;
-      })
-      
-      $(document).keypress(function(e){
-        if($(".dashboardZipSearch").is(":focus")){
-          code = (e.keyCode ? e.keyCode : e.which);
-          if (code == 13){
-            actionDashboardGo();
-          } 
+      $("#search-trigger").click(function(){
+        if($("#search").hasClass('hover')){
+          $("#search").removeClass('hover');
+        }
+        else if(!$("#search").hasClass('hover')){
+          $("#search").addClass('hover');
         }
       });
 
-			
+      if($("#signup-scroll-target").length > 0){
+        // only do signup scroll if its on the page
+
+        $(window).scroll(function() {
+            var browserbottom = $(window).scrollTop() + $(window).height();
+            var offset = $("#signup-scroll-target").offset();
+            var height = offset.top - 30;
+
+            //console.log("scrollTop: " + $(window).scrollTop() + " browserbottom: " + browserbottom + " height: " + height)
+
+            if (browserbottom >= height) {
+                $("#sign-up").removeClass('fixed');
+            } else {
+                $("#sign-up").addClass('fixed');
+            }
+        });
+
+        $("#sign-up-tab").click(function(){
+          var offset = $("#signup-scroll-target").offset();
+          var height = offset.top - 30;
+          $('html, body').animate({scrollTop: (height - $(window).height()) + 165},'fast');
+        });
+      }
+
+      $(".dashboardZipGo").click(function(){
+        actionDashboardGo();
+        return false;
+      });
+
+      $(document).keypress(function(e){
+        if($(".dashboardZipSearch").is(":focus")){
+          var code = (e.keyCode ? e.keyCode : e.which);
+          if (code === 13){
+            actionDashboardGo();
+          }
+        }
+      });
 
 
-			$("#listen-live").click(function(){
-				window.open($(this).attr('href'),'Listen_Live!','resizable=yes,scrollbars=yes,width=837,height=470');
-				return false;
-			});
-
-			$("#signup-form-submit-button").click(function(){
-				var form_data = $("#signup-form").serialize();
-				$("#signup-content").load("/bluehornet.php?" + form_data);
-				
-				_gaq.push(['_trackEvent', 'Signup', 'Footer Signup', $("#signup_name").val()+' '+$("#email_address").val()]);
-				
-				// set cookie
-				var now = new Date();
-				var time = now.getDate();
-				time += 365 * 20;
-				now.setDate(time);
-				document.cookie = '_signup_submitted=true; expires=' + now.toGMTString() + '; path=/';
-
-				
-				return false;
-			});
 
 
-			/**
-			 * Handles toggling the main navigation menu for small screens.
-			 */
+      $("#listen-live").click(function(){
+        window.open($(this).attr('href'),'Listen_Live!','resizable=yes,scrollbars=yes,width=837,height=470');
+        return false;
+      });
 
-			var $masthead = $( '#masthead' ),
-					timeout = false;
+      $("#signup-form-submit-button").click(function(){
+        var form_data = $("#signup-form").serialize();
+        $("#signup-content").load("/bluehornet.php?" + form_data);
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_trackEvent', 'Signup', 'Footer Signup', $("#signup_name").val()+' '+$("#email_address").val()]);
+
+        // set cookie
+        var now = new Date();
+        var time = now.getDate();
+        time += 365 * 20;
+        now.setDate(time);
+        document.cookie = '_signup_submitted=true; expires=' + now.toGMTString() + '; path=/';
+
+
+        return false;
+      });
+
+
+      /**
+       * Handles toggling the main navigation menu for small screens.
+       */
+
+      var $masthead = $( '#masthead' ),
+          timeout = false;
 
       // $.fn.smallMenu = function() {
       //  $masthead.find( '.site-navigation' ).removeClass( 'main-navigation' ).addClass( 'main-small-navigation' );
       //  $masthead.find( '.site-navigation h1' ).removeClass( 'assistive-text' ).addClass( 'menu-toggle' );
-      // 
+      //
       //  $( '.menu-toggle' ).unbind( 'click' ).click( function() {
       //    $masthead.find( '.menu' ).toggle();
       //    $( this ).toggleClass( 'toggled-on' );
       //  } );
       // };
 
-			// Check viewport width on first load.
-			if ( $( window ).width() < 600 ){
-				$.fn.smallMenu();
-				}
+      // Check viewport width on first load.
+      if ( $( window ).width() < 600 ){
+        $.fn.smallMenu();
+        }
 
-			// Check viewport width when user resizes the browser window.
-			$( window ).resize( function() {
-				var browserWidth = $( window ).width();
+      // Check viewport width when user resizes the browser window.
+      $( window ).resize( function() {
+        var browserWidth = $( window ).width();
 
-				if ( false !== timeout ){
-					clearTimeout( timeout );
-				}
+        if ( false !== timeout ){
+          clearTimeout( timeout );
+        }
 
-				timeout = setTimeout( function() {
-					if ( browserWidth < 600 ) {
-						$.fn.smallMenu();
-					} else {
-						$masthead.find( '.site-navigation' ).removeClass( 'main-small-navigation' ).addClass( 'main-navigation' );
-						$masthead.find( '.site-navigation h1' ).removeClass( 'menu-toggle' ).addClass( 'assistive-text' );
-						$masthead.find( '.menu' ).removeAttr( 'style' );
-					}
-				}, 200 );
-			} );
+        timeout = setTimeout( function() {
+          if ( browserWidth < 600 ) {
+            $.fn.smallMenu();
+          } else {
+            $masthead.find( '.site-navigation' ).removeClass( 'main-small-navigation' ).addClass( 'main-navigation' );
+            $masthead.find( '.site-navigation h1' ).removeClass( 'menu-toggle' ).addClass( 'assistive-text' );
+            $masthead.find( '.menu' ).removeAttr( 'style' );
+          }
+        }, 200 );
+      } );
 
 
     // ===============================================================
@@ -152,49 +202,8 @@
     });
 
 
-		
 
 
-		});
-	})(jQuery);
-	
-	function actionDashboardGo(){
-		 jQuery(".dashboardZipSearch").removeClass('error');
-		 var context;
-		 if(jQuery("li.dashboard").css('text-shadow') != 'none'){
-			context = jQuery("li.dashboard");
-		 }
-		 else{
-			context = jQuery("#action-dashboard");
-		 }
-			if(jQuery(".dashboardZipSearch", context).val() != '' && jQuery(".dashboardZipSearch", context).val() != "Enter your zip"){
-	       window.location = '/congress/' + jQuery(".dashboardZipSearch", context).val();
-	    }
-	    else{
-	
-	       jQuery(".dashboardZipSearch", context).addClass('error')
-	
-	    }
-	
-     
-   }
 
-   function getCookie(name){
-	var nameEQ = name + "=";
-		var ca = document.cookie.split(';');
-		for(var i=0;i < ca.length;i++) {
-			var c = ca[i];
-			while (c.charAt(0)==' ') c = c.substring(1,c.length);
-			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-		}
-		return null;
-   }
-   function setCookie(name, value, days){
-	 if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	 }
-	 else var expires = "";
-	 document.cookie = name+"="+value+expires+"; path=/";
-   }
+    });
+  })(jQuery);
