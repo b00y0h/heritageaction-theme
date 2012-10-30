e9678538b8bab7cf5103fa963bb67da82322b329
Bobby Smith <reversetype@gmail.com>
2012-10-30 16:49:24 -0400

    remove cruft. #primary bg to white. img width 100%

---
+++ page-search.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package Heritage Action
+ * @since Heritage Action 1.0
+ */
+
+ /*
+ Template Name: Search
+ */
+
+get_header(); ?>
+
+<!-- ========== -->
+<!-- = Search = -->
+<!-- ========== -->
+
+
+<?php get_footer(); ?>
