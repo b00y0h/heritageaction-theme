e6d10d1477bb7356f9875bcfb65e8eec2f33ed93
Bobby Smith <reversetype@gmail.com>
2012-10-30 11:15:30 -0400

    apple-mobile-web-app-capable

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes" />
 <title>Heritage Action</title>
 <link href='http://fonts.googleapis.com/css?family=IM+Fell+Great+Primer+SC|Open+Sans+Condensed:700|Bree+Serif' rel='stylesheet' type='text/css'>
 
