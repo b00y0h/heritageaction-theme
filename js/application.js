07a45daf934530dcc97e8e32c63bb9e6654a3a86
Bobby Smith <reversetype@gmail.com>
2012-11-07 09:43:31 -0500

    fix jshint error and minified new changes
    removed unused functions. placed in unused.js

--- js/application.js
+++ js/application.js
@@ -23,30 +23,6 @@
      }
   }
 
-  function getCookie(name){
- var nameEQ = name + "=";
-   var ca = document.cookie.split(';');
-   for(var i=0;i < ca.length;i++) {
-     var c = ca[i];
-     while (c.charAt(0) ===' ') {c = c.substring(1,c.length);}
-     if (c.indexOf(nameEQ) === 0) {return c.substring(nameEQ.length,c.length);}
-   }
-   return null;
-  }
-  function setCookie(name, value, days){
-    var expires;
-  if (days) {
-   var date = new Date();
-   date.setTime(date.getTime()+(days*24*60*60*1000));
-   expires = "; expires="+date.toGMTString();
-  }
-  else { expires = "";}
-  document.cookie = name+"="+value+expires+"; path=/";
-  }
-
-
-
-
   (function($){
     $(document).ready(function(){
 
@@ -204,7 +180,7 @@
 		// allow clicking on nav dropdown to act as link click
 		$("#menu-main-nav > li.score-card, #menu-main-nav > li.the-forge-blog, #menu-main-nav > li.donate").click(function(){
 			window.location.href = $('a',$(this)).attr('href');
-		})
+		});
 
 
 
