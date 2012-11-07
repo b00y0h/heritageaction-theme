0b0f33128e06ff02c420492156117190bfa1c417
Bobby Smith <reversetype@gmail.com>
2012-11-07 09:37:18 -0500

    switch to minified js

--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@ function heritageaction_setup() {
 	 * Add support for the Aside Post Formats
 	 */
 	add_theme_support( 'post-formats', array( 'aside', ) );
-	
+
 	// remove html tags
 	add_filter('comment_form_defaults','comments_form_defaults');
 }
@@ -118,7 +118,7 @@ function heritageaction_scripts() {
 	wp_enqueue_style( 'orangebox', get_template_directory_uri() ."/orangebox.css" );
 	//wp_enqueue_style( 'bxstyles', get_template_directory_uri() ."/bx_styles.css" );
 
-	wp_enqueue_script( 'application', get_template_directory_uri() . '/js/application.js', array( 'jquery' ), '20120928', true );
+	wp_enqueue_script( 'application', get_template_directory_uri() . '/js/application-ck.js', array( 'jquery' ), '20120928', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -127,13 +127,13 @@ function heritageaction_scripts() {
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
-	
+
 	wp_enqueue_script('jquery-ui-core');
 	wp_enqueue_script( 'jquery-effects-custom', get_template_directory_uri() . '/js/jquery-ui-1.8.24.custom.min.js', array( 'jquery' ), '20121002', true );
 	wp_enqueue_script( 'orangebox', get_template_directory_uri() . '/js/orangebox.min.js', array( 'jquery' ), '20121004');
 	wp_enqueue_script( 'tinycarousel', get_template_directory_uri() . '/js/jquery.tinycarousel.js', array( 'jquery' ), '20121004', true );
-	
-	
+
+
 }
 add_action( 'wp_enqueue_scripts', 'heritageaction_scripts' );
 
@@ -147,11 +147,11 @@ function amount_shortcode( $atts ){
   	$amt = str_replace($RemoveText,'',trim($_GET['amt']));
   	if (!is_numeric($amt)) { // If not numeric, just set to 0.
   		$amt = 0;
-  	} 
+  	}
 		else {
   		$amt = number_format($amt,2,'.','');
   	}
-  } 
+  }
 	else {
   	$amt = 0;
   }
@@ -174,10 +174,10 @@ add_shortcode( 'gf_count', 'gf_count');
 
 function post_key_vote_meta_box( $post ) {
   // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'key_vote_nonce' );  
+  wp_nonce_field( plugin_basename( __FILE__ ), 'key_vote_nonce' );
     $key_vote_type = get_post_meta($post->ID,'key_vote_type',true);
     ?>
-    
+
   <select id="key_vote_type" name="key_vote_type">
     <option  value="">Co Sponsorship</option>
     <option <?php echo ($key_vote_type == 'yes') ? 'selected="selected"' : '';  ?> value="yes">Yes</option>
@@ -193,16 +193,16 @@ function add_key_vote_metabox() {
 add_action( 'save_post', 'ha_save_key_vote_type' );
 function ha_save_key_vote_type( $post_id ) {
   $post = get_post($post_id);
-  
+
   if($post->post_type == 'key-votes'){
-    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
         return;
 
     if (isset($_POST['key_vote_nonce']) && !wp_verify_nonce( $_POST['key_vote_nonce'], plugin_basename( __FILE__ ) ) )
         return;
 
     // Check permissions
-    if ( 'page' == $_POST['post_type'] ) 
+    if ( 'page' == $_POST['post_type'] )
     {
       if ( !current_user_can( 'edit_page', $post_id ) )
           return;
@@ -215,7 +215,7 @@ function ha_save_key_vote_type( $post_id ) {
 
     update_post_meta($post_id, 'key_vote_type', $_POST['key_vote_type']);
   }
-  
+
 }
 
 // ==========================================================================
@@ -226,22 +226,22 @@ function ha_save_key_vote_type( $post_id ) {
 function search_url_rewrite_rule() {
     if (is_search() && !empty($_GET['s'])) {
         if(isset($_GET['search_post_type']) && $_GET['search_post_type'] == 'post'){
-          wp_redirect(home_url("/search-blog/") . urlencode(get_query_var('s')) );        
+          wp_redirect(home_url("/search-blog/") . urlencode(get_query_var('s')) );
         }
         else{
           wp_redirect(home_url("/search/") . urlencode(get_query_var('s')) );
-        }  
-          
+        }
+
         exit();
-    }   
+    }
 }
 add_action('template_redirect', 'search_url_rewrite_rule');
 
 add_action( 'init', 'search_rule' );
-function search_rule(){    
+function search_rule(){
       add_rewrite_rule('^search-blog/([^/]*)?', 'index.php?s=$matches[1]&post_type=post', 'top');
       add_rewrite_rule('^search/([^/]*)?', 'index.php?s=$matches[1]', 'top');
-    
+
 }
 
 /**
@@ -292,9 +292,9 @@ function twitter_feed($user = 'twitter', $count = '3'){
     // If the API returns a valid response, the data will be
     // json-encoded; so decode it.
     $data = json_decode( $remote['body'] );
-    
+
     $output = '';
-    
+
     while ($i <= $count){
         //Assign feed to $feed
         if(isset($data[$i-1])){
@@ -310,21 +310,21 @@ function twitter_feed($user = 'twitter', $count = '3'){
                 $feed = preg_replace("/@(\w+)/", "<b><a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a></b>", $feed);
                 $feed = preg_replace("/#(\w+)/", "<a href=\"http://search.twitter.com/search?q=\\1\" target=\"_blank\">#\\1</a>", $feed);
                 $output[$data[($i-1)]->created_at] = ''.
-                  "<li>" .  
-                    "<span class='meta'>" . 
-                      human_time_diff( strtotime($data[($i-1)]->created_at), current_time('timestamp', 1) ) . 
+                  "<li>" .
+                    "<span class='meta'>" .
+                      human_time_diff( strtotime($data[($i-1)]->created_at), current_time('timestamp', 1) ) .
                     "</span>" .
                     '<span class="excerpt">' . $feed . '</span>' .
                   "</li>";
         }
         $i++;
     }
-    
-    krsort($output);    
+
+    krsort($output);
     $output = implode("\n", $output);
-    
+
     set_transient( $transient_key, $output, 600 );
-    
+
     return $output;
 }
 
@@ -353,7 +353,7 @@ function truncateWords($string, $your_desired_width) {
 function reversetype_post_type_labels( $singular, $plural = '' )
 {
     if( $plural == '') $plural = $singular .'s';
-    
+
     return array(
         'name' => _x( $plural, 'post type general name', 'heritageaction' ),
         'singular_name' => _x( $singular, 'post type singular name', 'heritageaction' ),
@@ -386,7 +386,7 @@ function heritageaction_show_extra_profile_fields( $user ) { ?>
 	<h3>Heritage Action profile information</h3>
 
 	<table class="form-table">
-	  
+
 	  <tr>
 			<th><label for="twitter">Title</label></th>
 
@@ -395,7 +395,7 @@ function heritageaction_show_extra_profile_fields( $user ) { ?>
 				<span class="description">Please enter your job title.</span>
 			</td>
 		</tr>
-    
+
 
 		<tr>
 			<th><label for="twitter">Twitter</label></th>
