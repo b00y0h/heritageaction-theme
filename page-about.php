6af8ec13547c4ede21c14de16ec73d31c9df3223
Andre Fredette <andre@reality66.com>
2012-11-08 18:40:09 -0500

    Updated about page

--- page-about.php
+++ page-about.php
@@ -15,185 +15,52 @@ get_header(); ?>
 		      <div class="inner">
     		  <h1 id="about_us">About Us</h1>
 		      <div class="col1">
-  		      <p>This is a description of Heritage Action. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
-            <a href="#" class="btn rounded centered gradient red-gradient">SUPPORT HERITAGE</a>
+		        <a href="http://www.youtube.com/watch?v=xwLCw1oKQcg" rel="lightbox" data-ob_share="false">
+  		        <img src="<?php echo get_bloginfo('template_url'); ?>/img/video-image-large.png" alt="Slide1" class="size-full">
+  		      </a>
+  		      
+  		      <?php the_content(); ?>
+            
+            <p align="center"><a href="/donate" class="btn rounded centered gradient red-gradient">DONATE TO HERITAGE ACTION</a></p>
           </div>
+          
           </div> <!-- .inner -->
   		  </div> <!-- #hero -->
    <div id="main-inner">
 		<div id="primary" class="content-area">
   		  <h1>Meet The Team</h1>
   		  <div id="content" class="site-content row" role="main">
-          
-          <!-- #michael-needham -->
-          <section id="michael-needham" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-michael-needham.jpg" alt="Michael Needham Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Michael A. Needham</h2>
-              <h3 class="title">Chief Executive Officer</h3>
-              <p>Mike Needham is the Chief Executive Officer for Heritage Action for America. He is responsible for setting the strategy and vision of Heritage Action, ensuring it advances the mission of The Heritage Foundation while building it into a permanent presence in Washington. He also oversees all policy decisions that hold Members of Congress accountable.Prior to his role at Heritage Action, Mike served in four different roles at The Heritage Foundation, including Chief of Staff, advisor to the President, and a director in The Kathryn and Shelby Cullom Davis Institute for International Studies.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #michael-needham ^^^ -->
-          
-          <!-- #tim-chapman -->
-          <section id="tim-chapman" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-tim-chapman.jpg" alt="Tim Chapman Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Tim Chapman</h2>
-              <h3 class="title">Chief Operating Officer</h3>
-              <p>Tim Chapman is the Chief Operating Officer of Heritage Action for America. He oversees the day-to-day operations while working closely with our sister organization, The Heritage Foundation, to ensure both organizations work towards common goals.Prior to his role at Heritage Action, Tim previously served as Chief of Staff to Heritage Foundation President Ed J. Feulner, Ph.D. In this position, Tim advised Dr. Feulner on public policy matters and activities of the conservative movement. He also managed Dr. Feulner’s office staff and Heritage’s day-to-day operations.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #tim-chapman ^^^ -->
-          
-          <!-- #jessica-anderson -->
-          <section id="jessica-anderson" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-jessica-anderson.jpg" alt="Jessica Anderson Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Jessica Anderson</h2>
-              <h3 class="title">Deputy Political Director</h3>
-              <p>Jessica Anderson is a Deputy Political Director for Heritage Action for America. She first started with Heritage Action as the North Carolina State Director. Previously, she led the get-out-the-vote effort, campaign and new media training for the John W. Pope Civitas Institute in Raleigh, NC. Today, you can find her identifying, equipping and training activists for Heritage Action on the ground throughout the country. She is a University of Florida graduate.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #jessica-anderson ^^^ -->
-          
-          <!-- #tripp-baird -->
-          <section id="tripp-baird" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-tripp-baird.jpg" alt="Tripp Baird Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Tripp Baird</h2>
-              <h3 class="title">Director of Senate Relations</h3>
-              <p>Tripp Baird is the Director of Senate Relations for Heritage Action for America. He leads Heritage Action’s efforts in the Senate, pushing for conservative policies, providing conservative analysis of upcoming votes. Tripp also works with Senators and their staffs to introduce Heritage-inspired legislation that advance conservative principles.Prior to his work with Heritage Action, Tripp was a Partner at the Watts Consulting Group, leading business activities in corporate strategy and public affairs, with an emphasis on public policy matters before the U.S. Congress and the Bush Administration.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #tripp-baird ^^^ -->
-          
-          <!-- #dustin-carmack -->
-          <section id="dustin-carmack" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-dustin-carmack.jpg" alt="Dustin Carmack Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Dustin Carmack</h2>
-              <h3 class="title">Deputy Political Director</h3>
-              <p>Dustin Carmack is the House Relations Deputy for Heritage Action for America.  Previously, he held numerous positions at The Heritage Foundation before spending a year in Israel studying Hebrew and Arabic.  A Missouri native, Dustin is a graduate of Truman State University.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #dustin-carmack ^^^ -->
-          
-          <!-- #dwayne-carson -->
-          <section id="dwayne-carson" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-dwayne-carson.jpg" alt="Dwayne Carson Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Dwayne Carson</h2>
-              <h3 class="title">House Relations Deputy</h3>
-              <p>Dwayne Carson is the House Relations Deputy for Heritage Action for America. Previously, Dwayne served with the Senator Burr (R-NC) campaign as a Regional Political Director. He also worked in North Carolina politics as a consultant for Rep. Virginia Foxx (R-NC) building coalitions in the 5th Congressional district. Dwayne began his career working for Members of Congress as a Legislative Correspondent and Legislative Assistant. Carson holds a degree in Communications and Arts from Oral Roberts University.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #dwayne-carson ^^^ -->
-          
-          <!-- #dan-holler -->
-          <section id="dan-holler" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-dan-holler.jpg" alt="Dan Holler Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Dan Holler</h2>
-              <h3 class="title">Communications Director</h3>
-              <p>Dan Holler is the Communications Director for Heritage Action for America. He is responsible for Heritage Action’s media outreach, pushing for conservative policies, providing conservative insights on upcoming votes and explaining how and why Conservatives often break ranks from the Establishment. Dan also manages Heritage Action’s blog and social media outreach.Prior to his work with Heritage Action, Dan was the Deputy Director of Senate Relations for The Heritage Foundation, where he was responsible for educating Senators and their staffs about think tank’s latest research and policy recommendations, with an emphasis on energy and environment, taxes, health care and national security.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #dan-holler ^^^ -->
-          
-          <!-- #erin-kanoy-siefring -->
-          <section id="erin-kanoy-siefring" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-erin-kanoy-siefring.jpg" alt="Erin Kanoy Siefring Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Erin Kanoy Siefring</h2>
-              <h3 class="title">Director of House Relations</h3>
-              <p>Erin Kanoy Siefring is the Director of House Relations for Heritage Action for America. She leads Heritage Action’s efforts in the House of Representatives, pushing for conservative policies, providing conservative analysis of upcoming votes and holding Representatives accountable to conservative principles. Erin also works with Representatives and their staffs to introduce Heritage-inspired legislation that advance conservative principles.Prior to her work with Heritage Action, Erin served as the Director of House Relations at The Heritage Foundation, where she developed and implemented strategy for advancing conservative principles in the House of Representatives.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #erin-kanoy-siefring ^^^ -->
-          
-          <!-- #andrew-mcIndoe -->
-          <section id="andrew-mcIndoe" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-andrew-mcIndoe.jpg" alt="Andrew McIndoe Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Andrew McIndoe</h2>
-              <h3 class="title">Development Manager</h3>
-              <p>Andrew McIndoe is the Development Manager for Heritage Action for America. He leads Heritage Action’s fundraising efforts, ensuring the organization has the resources it needs to remain the tip of the conservative spear in Washington. Previously, Andrew was the Director of Development at the Bill of Rights Institute and Development Associate at The Fund for American Studies. He was Morton Blackwell’s intern at the Leadership Institute, a Sarah T. Herman Scholar at the Young America’s Foundation Reagan Ranch Center, and a development intern at The Heritage Foundation. Originally from North Carolina, Andrew graduated with high honors from Grove City College.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #andrew-mcIndoe ^^^ -->
-          
-          <!-- #josh-robbins -->
-          <section id="josh-robbins" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-josh-robbins.jpg" alt="Josh Robbins Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Josh Robbins</h2>
-              <h3 class="title">Government Relations Deputy</h3>
-              <p>Josh Robbins is a Government Relations Deputy for Heritage Action for America.  He earned a Bachelor’s degree in Economics and International Studies at Yale University.  Prior to graduation, he was a summer intern in Heritage’s Center for International Trade and Economics and at Heritage Action for America.  He was born in New York, NY and grew up in Lexington, KY.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #josh-robbins ^^^ -->
-          
-          <!-- #ashe-schow -->
-          <section id="ashe-schow" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-ashe-schow.jpg" alt="Ashe Schow Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Ashe Schow</h2>
-              <h3 class="title">Deputy Communications Director</h3>
-              <p>Ashe Schow is the Deputy Communications Director for Heritage Action for America. Previously, she wrote website copy based on search engine optimization. She has a background in drafting political donation messaging for conservative-leaning PACs, as well as managing grassroots outreach programs at the local and county level. A Florida Native, she graduated from Florida State University with a Bachelor of Arts in Creative Writing.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #ashe-schow ^^^ -->
-          
-          <!-- #russ-vought -->
-          <section id="russ-vought" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-russ-vought.jpg" alt="Russ Vought Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Russ Vought</h2>
-              <h3 class="title">Political Director</h3>
-              <p>Russ Vought is the Political Director of Heritage Action for America. He provides Heritage Action with key strategic advice on all political and policy issues of concern. He also manages Heritage Action’s issue campaigns and grassroots department, working to identify and empower key activists in order to help them influence elected officials.Before coming to Heritage Action, Russ spent 12 years on Capitol Hill, in both the House of Representatives and the Senate, specializing in efforts to control spending and limit the federal government. Most recently, he was the Policy Director for the House Republican Conference, chaired by Congressman Mike Pence (R-IN).</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #ashe-schow ^^^ -->
-          
-          <!-- #nathanael-yellis -->
-          <section id="nathanael-yellis" class='team-info'>
-            <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
-              <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-nathanael-yellis.jpg" alt="Nathanael Yellis Photo">
-            </div>
-            <div class="info-detail">
-              <h2>Nathanael Yellis</h2>
-              <h3 class="title">Deputy Political Director</h3>
-              <p>Nathanael Yellis is responsible for Heritage Action for America’s web efforts. He was formerly the Business Developer at Yellis & Foley CPAs, an accounting firm serving small and medium businesses in Southern Maine and Seacoast New Hampshire. A Maine native, Yellis is a graduate of Patrick Henry College and the Acton School of Business.</p>
-            </div> <!-- .info-detail -->            
-          </section>
-          <!-- ^^^ end #nathanael-yellis ^^^ -->
+  		    
+  		   	<?php
+  					global $query_string; // required
+  					global $post;
+  					$posts = query_posts('post_type=page&post_parent=' . $post->ID . '&sort_column=menu_order&order=ASC&posts_per_page=-1'); 
+  				?>
+
+
+  						<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  						  
+  						  
+  						  <!-- #<?php echo $post->post_name; ?> -->
+                <section id="<?php echo $post->post_name; ?>" class='team-info'>
+                  <?php if(file_exists(get_stylesheet_directory().'/img/about-'.$post->post_name.'.jpg')) : ?>
+                  <div class="partner-image shadow shadow-curl-left shadow-curl-right float-left">
+                    <img src="<?php echo get_bloginfo('template_url'); ?>/img/about-<?php echo $post->post_name; ?>.jpg" alt="<?php the_title(); ?> Photo">
+                  </div>
+                  <?php endif; ?>
+                  <div class="info-detail">
+                    <?php the_content(); ?>
+                  </div> <!-- .info-detail -->            
+                </section>
+                <!-- ^^^ end #<?php echo $post->post_name; ?> ^^^ -->
+                  						
+  							
+  						<?php endwhile; // end of the loop. ?>
+
+
+  				<?php wp_reset_query(); // reset the query ?>
+  		    
+        
           
           <br><br><br>
           
@@ -201,5 +68,8 @@ get_header(); ?>
 
 		</div><!-- #primary .content-area -->    
     </div> <!-- #main-inner -->
-       
+    <div id="signup-scroll-target"></div>
+    
+    
+   
 <?php get_footer(); ?>
