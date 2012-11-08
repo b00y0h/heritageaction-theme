19789e6686aad1e32d9cb0aae0a0bfbfaed83420
Bobby Smith <reversetype@gmail.com>
2012-11-08 17:32:10 -0500

    hard coded prepend/append imports

--- js/application.js
+++ js/application.js
@@ -132,35 +132,45 @@
     * MIT License
   */
   (function( win ){
-  	var doc = win.document;
+    var doc = win.document;
 
-  	// If there's a hash, or addEventListener is undefined, stop here
-  	if( !location.hash && win.addEventListener ){
+    // If there's a hash, or addEventListener is undefined, stop here
+    if( !location.hash && win.addEventListener ){
 
-  		//scroll to 1
-  		window.scrollTo( 0, 1 );
-  		var scrollTop = 1,
-  			getScrollTop = function(){
-  				return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-  			},
+        //scroll to 1
+        window.scrollTo( 0, 1 );
+        var scrollTop = 1,
+            getScrollTop = function(){
+                return win.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
+            },
 
-  			//reset to 0 on bodyready, if needed
-  			bodycheck = setInterval(function(){
-  				if( doc.body ){
-  					clearInterval( bodycheck );
-  					scrollTop = getScrollTop();
-  					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-  				}
-  			}, 15 );
+            //reset to 0 on bodyready, if needed
+            bodycheck = setInterval(function(){
+                if( doc.body ){
+                    clearInterval( bodycheck );
+                    scrollTop = getScrollTop();
+                    win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                }
+            }, 15 );
 
-  		win.addEventListener( "load", function(){
-  			setTimeout(function(){
-  				//at load, if user hasn't scrolled more than 20 or so...
-  				if( getScrollTop() < 20 ){
-  					//reset to hide addr bar at onload
-  					win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-  				}
-  			}, 0);
-  		} );
-  	}
+        win.addEventListener( "load", function(){
+            setTimeout(function(){
+                //at load, if user hasn't scrolled more than 20 or so...
+                if( getScrollTop() < 20 ){
+                    //reset to hide addr bar at onload
+                    win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
+                }
+            }, 0);
+        } );
+    }
   })( this );
+
+
+
+  // @codekit-prepend "backstretch.js"
+  // @codekit-prepend "jquery.mousewheel.js"
+  // @codekit-prepend "jquery.history.js"
+  // @codekit-prepend "core.string.js"
+  // @codekit-prepend "jquery.easing.1.3.js"
+  // @codekit-prepend "jquery.smartresize.js"
+  // @codekit-append "jquery.page.js"
