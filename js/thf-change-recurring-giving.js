b230fe26cb988506a79bd87c4cff68b2891951ef
Andre Fredette <andre@reality66.com>
2013-02-28 10:53:43 -0500

    Updated recurring giving script to actually work

--- js/thf-change-recurring-giving.js
+++ js/thf-change-recurring-giving.js
@@ -6,15 +6,27 @@ function GetSelectedDonation(){ // Figures out currently selected Kimbia value.
 	} else { // A regular array value is selected
 		var SelectedDonation = jQuery('input[name=DonationLevel]:checked').val().replace(/\$|\,/g,''); // replace strips out dollar signs and commas
 	}
+	  
 	return SelectedDonation;
 }
 
-function RecurringGiving(){
+function ChangeText()
+{   
+    jQuery('.monthlyamt').html('$'+GetSelectedDonation());     
+}
+
+function RecurringGiving(){  
+  
+  jQuery('.groupLabel.section.k_askArrayMain.k_donation').text("Donation Amount");
+	jQuery('.k_paymentPlanTitle.required').text("Choose Your Donation Type");
 	jQuery('label.k_ongoing').after(jQuery('input.k_oneTime')); // Rearrange order of elements
-	jQuery('input.k_oneTime').after(jQuery('label.k_oneTime'));
+	jQuery('input.k_oneTime').after(jQuery('label.k_oneTime'));	
 	
-	jQuery('input.k_ongoing').attr('checked','checked'); // Check the ongoing box
+	jQuery('input.k_ongoing').trigger("click");//kimbia requires this radio button to be clicked
 	jQuery('select.k_payPlan option[value=MONTHLY]').attr('selected','selected'); // Select the monthly option
 	jQuery('select.k_payPlan').hide(); // Hide the drop-down option
-	jQuery('label.k_ongoing').replaceText(/ongoing payments./,'Monthly recurring donation to The Heritage Foundation (your card will be charged <span class="monthlyamt">$'+GetSelectedDonation()+'</span> each month) '); // Change the text
+	jQuery('label.k_ongoing').replaceText(/ongoing payments./,'<span class="replace-text" style="display: inline;">Monthly recurring donation to Heritage Action (your card will be charged <span class="monthlyamt" style="display: inline; font-weight:bold;">$'+GetSelectedDonation()+'</span> each month</span>) '); // Change the text	
+	jQuery('.k_paymentPlanHint.k_ongoing').hide();
+	jQuery(".k_radioCB").change(function() {ChangeText();});
+	jQuery(".k_money.k_otherMoney").focusout(function() {console.log('focusout'); ChangeText();});  
 }
